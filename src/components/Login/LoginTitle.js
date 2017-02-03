f5fbb664008fdec946492677ad8d930f56bd5d2c
Liam Crewe <liam.j.crewe@gmail.com>
2017-02-03 22:33:11 +0000

    LoginTitle component, forgot to add to last commit

---
+++ src/components/Login/LoginTitle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginTitle = () => (
+  <div className="login-title-div">
+    <div className="login-title">Log in to SelfReflect</div>
+    <br />
+    Enter your email and password below to log in.
+  </div>
+)
+
+export default LoginTitle
