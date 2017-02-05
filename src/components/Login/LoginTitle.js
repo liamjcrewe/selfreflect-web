ee9eada45c278e46b4fb26a21335ba6140c9eb49
Liam Crewe <liam.j.crewe@gmail.com>
2017-02-05 18:33:54 +0000

    No need to name/export this - just export function

--- src/components/Login/LoginTitle.js
+++ src/components/Login/LoginTitle.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const LoginTitle = () => (
+export default () => (
   <div className="login-title-div">
     <div className="login-title">Log in to SelfReflect</div>
     <br />
     Enter your email and password below to log in.
   </div>
 )
-
-export default LoginTitle
