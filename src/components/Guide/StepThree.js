9dbd3840322ea1708c881d77419de015e4fd0bd2
Liam Crewe <liam.j.crewe@gmail.com>
2017-03-02 11:40:46 +0000

    Implemented guide page

---
+++ src/components/Guide/StepThree.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => (
+  <div className="guide-step">
+    <div className="guide-title">Step 3) Analyse your data</div>
+    <div className="guide-text">
+      Go to the 'Analyse' tab and look at relationships between your mental wellbeing and data from existing apps.
+    </div>
+  </div>
+)
---
+++ src/components/Guide/StepTwo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => (
+  <div className="guide-step">
+    <div className="guide-title">Step 2) Record your wellbeing</div>
+    <div className="guide-text">
+      Go to the 'Record' tab and record your wellbeing. Also be sure to download out mobile app to record your wellbeing from your phone!
+    </div>
+  </div>
+)
