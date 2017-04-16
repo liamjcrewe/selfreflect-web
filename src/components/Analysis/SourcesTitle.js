37206faa482e0eadaafbd6f6fe129a046c0f3ca2
Liam Crewe <liam.j.crewe@gmail.com>
2017-04-16 21:25:32 +0100

    Improved sources title message for clarity

--- src/components/Analysis/SourcesTitle.js
+++ src/components/Analysis/SourcesTitle.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default () => (
   <div className="sources-title-div">
-    <div className="sources-title">Select your sources</div>
+    <div className="sources-title">Select your options from the following sources</div>
     <br />
     Note that while you can select any of the sources, they will only work if you have connected the corresponding app in the 'Account' tab.
   </div>
